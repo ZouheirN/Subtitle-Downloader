88f07f9630a687fd9c9ea74439bf9f8574538005
Zouheir Nakouzi <zouheir2002@gmail.com>
2024-08-14 18:44:40 +0300

    fixed small scrolling bug

--- lib/features/subtitles/ui/downloaded_subtitles_history_page.dart
+++ lib/features/subtitles/ui/downloaded_subtitles_history_page.dart
@@ -30,115 +30,111 @@ class _DownloadedSubtitlesHistoryPageState
           var subtitles = DownloadedSubtitlesBox.getAllDownloadedSubtitles();
           var movieNames = subtitles.map((e) => e.keys.first).toList();
 
-          return Column(
-            children: [
-              ListView.builder(
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return Dismissible(
-                    key: Key(movieNames[index]),
-                    onDismissed: (direction) async {
-                      await DownloadedSubtitlesBox.deleteDownloadedSubtitle(
-                          subtitles[index].values.first.first['url'], false);
+          return ListView.builder(
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: Key(movieNames[index]),
+                onDismissed: (direction) async {
+                  await DownloadedSubtitlesBox.deleteDownloadedSubtitle(
+                      subtitles[index].values.first.first['url'], false);
 
-                      final removedMovieName = movieNames[index];
+                  final removedMovieName = movieNames[index];
 
-                      setState(() {
-                        subtitles =
-                            DownloadedSubtitlesBox.getAllDownloadedSubtitles();
-                        movieNames =
-                            subtitles.map((e) => e.keys.first).toList();
-                      });
+                  setState(() {
+                    subtitles =
+                        DownloadedSubtitlesBox.getAllDownloadedSubtitles();
+                    movieNames =
+                        subtitles.map((e) => e.keys.first).toList();
+                  });
 
-                      if (!context.mounted) return;
-                      ScaffoldMessenger.of(context).clearSnackBars();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('$removedMovieName Deleted'),
-                        ),
-                      );
-                    },
-                    background: Container(color: Colors.red),
-                    child: ExpansionTile(
-                      title: Text(movieNames[index]),
-                      subtitle: Text(
-                          'Total Subtitles: ${subtitles[index][movieNames[index]]!.length}'),
-                      children: subtitles[index][movieNames[index]]!
-                          .map<Widget>((e) =>
-                              BlocListener<SubtitleBloc, SubtitleState>(
-                                bloc: subtitleBloc,
-                                listenWhen: (previous, current) =>
-                                    current is SubtitleActionState,
-                                listener: (context, state) {
-                                  switch (state.runtimeType) {
-                                    case const (SubtitleDownloadPermissionNotGrantedState):
-                                      ScaffoldMessenger.of(context)
-                                          .clearSnackBars();
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        const SnackBar(
-                                          content: Text(
-                                              'Write Permissions Were Not Granted'),
-                                        ),
-                                      );
-                                      break;
-                                    case const (SubtitleDownloadStartedState):
-                                      ScaffoldMessenger.of(context)
-                                          .clearSnackBars();
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        const SnackBar(
-                                          content:
-                                              Text('Downloading Subtitle...'),
-                                        ),
-                                      );
-                                      break;
-                                    case const (SubtitleDownloadSuccessState):
-                                      ScaffoldMessenger.of(context)
-                                          .clearSnackBars();
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        const SnackBar(
-                                          content: Text('Subtitle Downloaded'),
-                                        ),
-                                      );
-                                      break;
-                                    case const (SubtitleDownloadErrorState):
-                                      ScaffoldMessenger.of(context)
-                                          .clearSnackBars();
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        const SnackBar(
-                                          content:
-                                              Text('Subtitle Download Failed'),
-                                        ),
-                                      );
-                                      break;
-                                  }
-                                },
-                                child: ListTile(
-                                  title: Text(e['releaseName']),
-                                  subtitle: Text('Uploader: ${e['author']}'),
-                                  onTap: () {
-                                    // download subtitle
-                                    subtitleBloc.add(SubtitleDownloadEvent(
-                                      e['url'],
-                                      e['releaseName'],
-                                      e['author'],
-                                      movieNames[index],
-                                      movieNames[index],
-                                      'movie',
-                                    ));
-                                  },
-                                ),
-                              ))
-                          .toList(),
+                  if (!context.mounted) return;
+                  ScaffoldMessenger.of(context).clearSnackBars();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('$removedMovieName Deleted'),
                     ),
                   );
                 },
-                itemCount: subtitles.length,
-              ),
-            ],
+                background: Container(color: Colors.red),
+                child: ExpansionTile(
+                  title: Text(movieNames[index]),
+                  subtitle: Text(
+                      'Total Subtitles: ${subtitles[index][movieNames[index]]!.length}'),
+                  children: subtitles[index][movieNames[index]]!
+                      .map<Widget>((e) =>
+                          BlocListener<SubtitleBloc, SubtitleState>(
+                            bloc: subtitleBloc,
+                            listenWhen: (previous, current) =>
+                                current is SubtitleActionState,
+                            listener: (context, state) {
+                              switch (state.runtimeType) {
+                                case const (SubtitleDownloadPermissionNotGrantedState):
+                                  ScaffoldMessenger.of(context)
+                                      .clearSnackBars();
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(
+                                    const SnackBar(
+                                      content: Text(
+                                          'Write Permissions Were Not Granted'),
+                                    ),
+                                  );
+                                  break;
+                                case const (SubtitleDownloadStartedState):
+                                  ScaffoldMessenger.of(context)
+                                      .clearSnackBars();
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(
+                                    const SnackBar(
+                                      content:
+                                          Text('Downloading Subtitle...'),
+                                    ),
+                                  );
+                                  break;
+                                case const (SubtitleDownloadSuccessState):
+                                  ScaffoldMessenger.of(context)
+                                      .clearSnackBars();
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Subtitle Downloaded'),
+                                    ),
+                                  );
+                                  break;
+                                case const (SubtitleDownloadErrorState):
+                                  ScaffoldMessenger.of(context)
+                                      .clearSnackBars();
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(
+                                    const SnackBar(
+                                      content:
+                                          Text('Subtitle Download Failed'),
+                                    ),
+                                  );
+                                  break;
+                              }
+                            },
+                            child: ListTile(
+                              title: Text(e['releaseName']),
+                              subtitle: Text('Uploader: ${e['author']}'),
+                              onTap: () {
+                                // download subtitle
+                                subtitleBloc.add(SubtitleDownloadEvent(
+                                  e['url'],
+                                  e['releaseName'],
+                                  e['author'],
+                                  movieNames[index],
+                                  movieNames[index],
+                                  'movie',
+                                ));
+                              },
+                            ),
+                          ))
+                      .toList(),
+                ),
+              );
+            },
+            itemCount: subtitles.length,
           );
         },
       ),
